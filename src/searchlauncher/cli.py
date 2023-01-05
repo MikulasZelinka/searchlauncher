e1bbdc7ecbdb9c0ea67935917c23f1c3b0d44ad8
Mikuláš Zelinka <mikulas@zelinka.dev>
2023-01-05 22:33:36 +0100

    fix: mypy ignore backport StrEnum

--- src/searchlauncher/cli.py
+++ src/searchlauncher/cli.py
@@ -3,7 +3,7 @@ import sys
 if sys.version_info >= (3, 11):
     from enum import StrEnum
 else:
-    from backports.strenum import StrEnum
+    from backports.strenum import StrEnum  # type: ignore
 from typing import Generator
 
 import typer
